8e46020487103362257e707de0b569844319c0a9
zanpen2000 <zanpen2000@gmail.com>
2014-01-26 14:41:53 +0800

    Merge branch 'master' of https://github.com/zanpen2000/TaskFramework into peng-master
    Conflicts:
    	TaskFramework.Lib/ParallelTaskQueue.cs
    	TaskFramework.Lib/SimpleParallelTask.cs

--- TaskFramework.Lib/SimpleParallelTask.cs
+++ TaskFramework.Lib/SimpleParallelTask.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace TaskFramework.Lib
 {
+    /// <summary>
+    /// 简单并行任务
+    /// </summary>
     public class SimpleParallelTask : IParallelTask
     {
         #region 字段和属性
