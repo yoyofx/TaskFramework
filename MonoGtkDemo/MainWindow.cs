a693449b743d7b45f7d239754dd70f841a49d613
zanpen2000 <zanpen2000@gmail.com>
2014-01-28 07:06:02 +0800

    增加ListView组建的列绘制方法，并在窗体启动时调用。

--- MonoGtkDemo/MainWindow.cs
+++ MonoGtkDemo/MainWindow.cs
@@ -6,6 +6,7 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		buildListView();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -13,4 +14,34 @@ public partial class MainWindow: Gtk.Window
 		Application.Quit ();
 		a.RetVal = true;
 	}
+
+
+	protected void RunTestClicked(object sender, EventArgs e)
+	{
+
+
+
+	}
+	#region private methods
+
+	void buildListView()
+	{
+		Gtk.TreeViewColumn colId = new TreeViewColumn();
+		colId.Title = "Task Id";
+
+		Gtk.TreeViewColumn colProgress = new TreeViewColumn();
+		colProgress.Title = "Progress";
+
+		Gtk.TreeViewColumn colDelay = new TreeViewColumn();
+		colDelay.Title = "Delay(ms)";
+
+		listView.AppendColumn(colId);
+		listView.AppendColumn(colProgress);
+		listView.AppendColumn(colDelay);
+
+		Gtk.ListStore mlist = new ListStore(typeof(string), typeof(string),typeof(string));
+		listView.Model = mlist;
+	}
+
+	#endregion
 }
